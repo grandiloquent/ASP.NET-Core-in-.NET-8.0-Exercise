27744cafb86732d6dd2254d007139a2b48089576
grandiloquent <1785907865@qq.com>
2024-08-10 04:06:48 +0800

    更新

--- TextRecognition/Program.cs
+++ TextRecognition/Program.cs
@@ -1,4 +1,9 @@
-﻿KeyboardShare kbh = new KeyboardShare();
+﻿using System.Drawing;
+using static Screenshot;
+
+POINT _point1 = new POINT();
+POINT _point2 = new POINT();
+KeyboardShare kbh = new KeyboardShare();
 kbh.ConfigHook();
 kbh.KeyDown += async (s, k) =>
 {
@@ -7,11 +12,23 @@ kbh.KeyDown += async (s, k) =>
     {
         case Key.F1:
             {
-
+                if (_point1.X == 0)
+                {
+                    GetCursorPos(out _point1);
+                }
+                else
+                {
+                    GetCursorPos(out _point2);
+                    var value = ImageUtils.Ocr(new Point(_point1.X, _point1.Y), new Point(_point2.X, _point2.Y));
+                    ClipboardShare.SetText(value);
+                    _point1 = new POINT();
+                    _point2 = new POINT();
+                }
                 break;
             }
         case Key.F2:
             {
+                Utils.Translate();
                 break;
             }
         case Key.F3:
--- TextRecognition/Shared/Utils.cs
+++ TextRecognition/Shared/Utils.cs
@@ -1,3 +1,49 @@
-public class Utils{
-    
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+public class Utils
+{
+    public static string Translate(string s = "")
+    {
+        //string q
+        // http://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=%s&dt=t&dt=bd&ie=UTF-8&oe=UTF-8&dj=1&source=icon&q=
+        // en
+        // http://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&dt=bd&ie=UTF-8&oe=UTF-8&dj=1&source=icon&q=
+        var l = "en";
+        s = s == "" ? ClipboardShare.GetText() : s;
+
+        var isChinese = Regex.IsMatch(s, "[\u4e00-\u9fa5]");
+        if (!isChinese)
+        {
+            l = "zh";
+        }
+        var req = WebRequest.Create(
+                      "http://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=" + l + "&dt=t&dt=bd&ie=UTF-8&oe=UTF-8&dj=1&source=icon&q=" +
+                      s);
+        //req.Proxy = new WebProxy("127.0.0.1", 10809);
+        var res = req.GetResponse();
+        using (var reader = new StreamReader(res.GetResponseStream()))
+        {
+            var obj =
+          (JsonElement)JsonSerializer.Deserialize<Dictionary<String, dynamic>>(reader.ReadToEnd())["sentences"];
+            //var obj = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd())["sentences"].ToObject<JArray>();
+            var sb = new StringBuilder();
+            for (int i = 0; i < obj.GetArrayLength(); i++)
+            {
+                sb.Append(obj[i].GetProperty("trans").GetString()).Append(' ');
+            }
+            // Regex.Replace(sb.ToString().Trim(), "[ ](?=[a-zA-Z0-9])", m => "_").ToLower();
+            // std::string {0}(){{\n}}
+            //return string.Format("{0}", Regex.Replace(sb.ToString().Trim(), " ([a-zA-Z0-9])", m => m.Groups[1].Value.ToUpper()).Decapitalize());
+            //return  sb.ToString().Trim();
+            /*
+			 sb.ToString().Trim();
+			 .Trim().Camel().Capitalize()
+			 */
+            return isChinese ? sb.ToString() : sb.ToString();
+        }
+        //Clipboard.SetText(string.Format(@"{0}", TransAPI.Translate(Clipboard.GetText())));
+    }
 }
